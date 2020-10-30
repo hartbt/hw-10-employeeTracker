4dcfd8f21009938099283cfa9dd325418fd9b5df
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-29 20:46:33 -0600

    Adding a role column into the seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,5 @@
 INSERT INTO department (name)
-VALUES ("HR"), ("Accounting"), ("Legal"), ("HR"), ("Sales")
+VALUES ("HR"), ("Accounting"), ("Legal"), ("Marketing"), ("Sales")
+
+INSERT INTO role (position, salary, departmentID)
+VALUE ("HR Representitive", 250000, 1), ("Head Accountant", 150000, 2), ("Attorney", 300000, 3), ("Marketing Director", 295000, 4), ("President of Sales", 500000, 5)
