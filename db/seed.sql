1307d8b6482a9f96b5e87efe17f943c6f5d0b54e
Brendon Hart <brendonhartbusiness@gmail.com>
2020-10-29 20:47:38 -0600

    Added semicolons

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,7 @@
 INSERT INTO department (name)
-VALUES ("HR"), ("Accounting"), ("Legal"), ("Marketing"), ("Sales")
+VALUES ("HR"), ("Accounting"), ("Legal"), ("Marketing"), ("Sales");
 
 INSERT INTO role (position, salary, departmentID)
-VALUE ("HR Representitive", 250000, 1), ("Head Accountant", 150000, 2), ("Attorney", 300000, 3), ("Marketing Director", 295000, 4), ("President of Sales", 500000, 5)
+VALUE ("HR Representitive", 250000, 1), ("Head Accountant", 150000, 2), ("Attorney", 300000, 3), ("Marketing Director", 295000, 4), ("President of Sales", 500000, 5);
+
+INSERT INTO employee ()
